ac65e914fd4dce442c4e1cb78657133e3470be36
Blueberryboom <152311332+Blueberryboom@users.noreply.github.com>
2026-01-29 23:10:54 +0000

    Modify startup logs and add interval logging
    Updated log messages and added a heartbeat check.

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-console.log("Node started");
+console.log("Node is running");
+console.log("Node version:", process.version);
 console.log("Token present:", !!process.env.DISCORD_TOKEN);
 
-console.log("Bot starting...");
-
-const { Client, GatewayIntentBits, Events } = require("discord.js");
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
-client.once(Events.ClientReady, () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+setInterval(() => {
+  console.log("Still alive");
+}, 5000);
